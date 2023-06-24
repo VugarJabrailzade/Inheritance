a6e7921b0bf646664900ecce77e2203b3b9c244f
VugarJabrailzade <135731013+VugarJabrailzade@users.noreply.github.com>
2023-06-24 16:48:52 +0400

    Add files via upload

---
+++ Inheritance/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+    namespace Inheritance
+{
+    public class Person
+    {
+        public string  Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname; 
+            Age = age;
+
+        }
+        public void GetInormation()
+        {
+            Console.WriteLine("Keep going to study");
+        }
+
+    }
+
+    public class Teacher : Person
+    {
+        public Teacher(string name, string surname, int age) : base(name, surname,age) { }
+        public void Explain()
+        {
+            Console.WriteLine("Explaining...");
+        }
+
+    }
+    public class Student : Person
+    {
+       
+        public Student(string name, string surname, int age) : base(name, surname, age) {}
+        public void Learn()
+        {
+           Console.WriteLine("Learning...");
+          
+        }
+    }
+
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Teacher teacher = new("Sara", "Aliyeva",42);
+            Student student = new("Yaver","Rovshenli",16);
+
+            Console.WriteLine(student.Name + " " + student.Surname + " " + student.Age);
+            student.Learn();
+           
+            Console.WriteLine(teacher.Name + " " + teacher.Surname + " " + teacher.Age);
+            teacher.Explain();
+            
+            student.GetInormation();
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
